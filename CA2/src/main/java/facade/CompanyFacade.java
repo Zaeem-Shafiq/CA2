17dbe79ab3a5a6d3a16bbf5ea627955ee83f62a4
Zaeem-Shafiq <zaeem1996@hotmail.com>
2017-03-17 10:24:44 +0100

    Added CRUD methods in CompanyFacade

--- CA2/src/main/java/facade/CompanyFacade.java
+++ CA2/src/main/java/facade/CompanyFacade.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
 import javax.persistence.TypedQuery;
 
 public class CompanyFacade {
@@ -25,13 +26,14 @@ public class CompanyFacade {
 //        System.out.println(cf.getCompanyByCvr(45313913).toString());
 //        System.out.println(cf.getCompanyByPhoneNumber("45942644").toString());
 //        System.out.println(cf.getCompaniesWithEmployeesMoreThan(200).toString());
+//        System.out.println(cf.deleteCompany(2));
     }
 
     public Company getCompanyById(int id) {
         EntityManager em = getEntityManager();
         return em.find(Company.class, id);
     }
-    
+
     public Company getCompanyByCvr(int cvr) {
         EntityManager em = getEntityManager();
         TypedQuery<Company> query = em.createQuery("SELECT c FROM Company c WHERE c.cvr = :cvr", Company.class);
@@ -51,5 +53,52 @@ public class CompanyFacade {
         TypedQuery<Company> query = em.createQuery("SELECT c FROM Company c WHERE c.numEmployees > :employees", Company.class);
         query.setParameter("employees", employees);
         return query.getResultList();
+    }
+
+    public Company createCompany(Company company) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(company);
+            em.getTransaction().commit();
+        } catch (RollbackException r) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return getCompanyById(company.getId());
+    }
+
+    public Company updateCompany(Company company) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(company);
+            em.getTransaction().commit();
+        } catch (RollbackException r) {
+            r.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return getCompanyById(company.getId());
+    }
+
+    public boolean deleteCompany(int id) {
+        EntityManager em = getEntityManager();
+        Company company = getCompanyById(id);
+        try {
+            em.getTransaction().begin();
+            company = em.merge(company);
+            em.remove(company);
+            em.getTransaction().commit();
+        } catch (RollbackException r) {
+            r.printStackTrace();
+            em.getTransaction().rollback();
+            return false;
+        } finally {
+            em.close();
+        }
+        return true;
     }
 }
