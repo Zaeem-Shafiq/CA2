5f6f7235ad5330ffe20460eba92b92d61536d691
Joacim <Joacim@vetterlain.dk>
2017-03-19 17:00:15 +0100

    added dokumentation.html

--- CA2/src/main/webapp/newjavascript.js
+++
@@ -1,8 +0,0 @@
-fetch('flowers.jpg')
-.then(function(response) {
-  return response.blob();
-})
-.then(function(myBlob) {
-  var objectURL = URL.createObjectURL(myBlob);
-  myImage.src = objectURL;
-});
